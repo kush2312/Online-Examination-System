866e1ace891cf4cd89bf0292a547973f6c2cb96e
kush2312 <kushdabade23@gmail.com>
2022-04-26 19:16:17 +0530

    Added code files

---
+++ admin/config/constants.php
@@ -0,0 +1,7 @@
+<?php 
+    define('SITEURL','http://localhost:80/Exam-system/');
+    define('LOCALHOST','localhost');
+    define('USERNAME','root');
+    define('PASSWORD','');
+    define('DBNAME','quizapp');
+?>
